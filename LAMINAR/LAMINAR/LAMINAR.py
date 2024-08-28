5d214f9cc7661209b3f04736eca4dc3f69d31e44
CKleiber <christian.kleiber01@gmail.com>
2024-08-28 21:26:51 +0200

    Not really working great...

--- LAMINAR/LAMINAR/LAMINAR.py
+++ LAMINAR/LAMINAR/LAMINAR.py
@@ -6,6 +6,7 @@ from scipy.spatial import KDTree
 from scipy.sparse.csgraph import dijkstra
 from scipy.stats import shapiro, combine_pvalues    
 from pingouin import multivariate_normality
+from tqdm import tqdm
 from LAMINAR.Flow.planarCNF import PlanarCNF, train_PlanarCNF
 from LAMINAR.utils.gaussian2uniform import sphere_to_gaussian, jacobian_gaussian_to_sphere
 
@@ -50,7 +51,7 @@ class LAMINAR():
         self.batch_size = hyperparameters.get('batch_size', 128)
 
         # make grid in unit sphere with 100 points in every dimension
-        self._make_grid(self.grid_resolution)
+        self._make_grid()
 
         # initialize the flow
         self.flow = PlanarCNF(in_out_dim=self.dimension, hidden_dim=self.hidden_dim, width=self.width, device=self.device)
@@ -59,34 +60,33 @@ class LAMINAR():
         # train the flow on data
         self._train(self.data, optimizer, epochs=epochs, batch_size=self.batch_size, patience=self.patience, sig=self.sig)
         
-        # push the data throught the flow
-        self.data_pushed = self.flow.transform(self.data, timesteps=self.timesteps)
-        
-        # pull back of grid
-        self.grid = self.flow.transform(self.grid_pushed, timesteps=self.timesteps, reverse=True)
-        
-        # generate the distance matrix of the grid
+        # concat the data and the grid
+        self.reference = torch.cat((self.data, self.grid), dim=0)
+        # push reference
+        self.reference_pushed = self.flow.transform(self.reference, timesteps=self.timesteps)
+        self.data_pushed = self.reference_pushed[:self.data.shape[0]]
+
+        # generate distance matrix
         self._generate_distance_matrix()
 
-    def _make_grid(self, grid_resolution: int):
-        '''
-        grid_resolution: int - resolution of the grid
-        '''
-        # create a grid from -1 to 1 with grid_resolution points in every dimension
-        grid = np.linspace(-1, 1, grid_resolution)
-        grid = np.array(np.meshgrid(*[grid for _ in range(self.dimension)])).reshape(self.dimension, -1).T
-        grid = torch.tensor(grid, dtype=torch.float32)
 
-        # filter out everything outside the unit sphere
-        grid = grid[torch.norm(grid, dim=1) < 1]
+    def _make_grid(self):
+        # make a grid spaning over the entire data range
 
-        self.grid_pushed = grid.to(self.device).float()
+        # get min and max values of the data in each dimension
+        min_values = torch.min(self.data, dim=0)[0]
+        max_values = torch.max(self.data, dim=0)[0]
 
-        # get 3**dimension neighbours for each point in the grid
-        self.KDTree_grid_pushed = KDTree(self.grid_pushed.cpu().detach().numpy())
-        _, self.grid_pushed_indices = self.KDTree_grid_pushed.query(self.grid_pushed.cpu().detach().numpy(), k=3**self.dimension)
+        # get the range of the data
+        range_values = max_values - min_values
 
-        
+        # number of grid points in each dimension 
+        grid_points = [int(self.grid_resolution * range_value.item()) for range_value in range_values]
+
+        # make uniform grid with grid_points points in each dimension
+        grid = torch.meshgrid(*[torch.linspace(min_values[i], max_values[i], grid_points[i]) for i in range(self.dimension)], indexing='ij')
+        self.grid = torch.stack(grid, dim=-1).reshape(-1, self.dimension).to(self.device)
+
     # get p value of the gaussian after the flow
     def p_value(self):
         '''
@@ -132,160 +132,97 @@ class LAMINAR():
     # function to generate the distance matrix for the neighbourhoods
     def _generate_distance_matrix(self):
         # jacobians
-        self.jacobians = np.zeros((self.grid.shape[0], self.dimension, self.dimension))
-        for i in range(self.grid.shape[0]):
-            J = self.jacobian(self.grid[i].reshape(1, -1))
-            self.jacobians[i] = J.cpu().detach().numpy()
-        self.jacobians = torch.tensor(self.jacobians, dtype=torch.float32).to(self.device)
+        pbar = tqdm(total=self.reference.shape[0], desc='Calculating Jacobians')
+        self.jacobians = torch.zeros(self.reference.shape[0], self.dimension, self.dimension).to(self.device)
+        for i in range(self.reference.shape[0]):
+            self.jacobians[i] = self.jacobian(self.reference[i].reshape(1, -1))
+            pbar.update(1)
+        self.metric_t = torch.einsum('ijk, ikl -> ijl', self.jacobians, self.jacobians).to(self.device)
+
+        # get neighbours of the reference points
+        pbar = tqdm(total=self.reference.shape[0], desc='Calculating Neighbours')
+        self.KDTree_reference_pushed = KDTree(self.reference_pushed.cpu().detach().numpy())
+        self.reference_pushed_indices = []
+        for i in range(self.reference_pushed.shape[0]):
+            _, indices = self.KDTree_reference_pushed.query(self.reference_pushed[i].cpu().detach().numpy(), k=self.k_neighbours)
+            self.reference_pushed_indices.append(indices)
+            pbar.update(1)
 
         # generate matrix and fill with inf
-        self.distance_matrix = torch.zeros((self.grid.shape[0], self.grid.shape[0])).to(self.device)
+        self.distance_matrix = torch.zeros((self.reference.shape[0], self.reference.shape[0])).to(self.device)
         self.distance_matrix.fill_(float('inf'))
 
         # fill non infty values
-        for i in range(self.grid.shape[0]):
-            for j in self.grid_pushed_indices[i]:
+        pbar = tqdm(total=self.reference.shape[0], desc='Calculating Distances')
+        for i in range(self.reference.shape[0]):
+            for j in self.reference_pushed_indices[i]:
                 if self.distance_matrix[i, j] == float('inf'):
-                    common_cov = (self.jacobians[i] + self.jacobians[j])/2 + 1e-6*torch.eye(self.dimension).to(self.device)
+                    # add small value for numerical stability
+                    common_metric_t = (self.metric_t[i] + self.metric_t[j])/2 + 1e-6*torch.eye(self.dimension).to(self.device)
 
-                    x_i = self.grid[i].reshape(1, -1)
-                    x_j = self.grid[j].reshape(1, -1)
+                    x_i = self.reference[i].reshape(1, -1)
+                    x_j = self.reference[j].reshape(1, -1)
 
-                    cov_det = torch.det(common_cov) ** 1/self.dimension
+                    met_det = torch.det(common_metric_t) ** 1/self.dimension
 
-                    mahalanobis_distance = torch.sqrt(cov_det * (x_i - x_j) @ torch.inverse(common_cov) @ (x_i - x_j).T)
+                    mahalanobis_distance = torch.sqrt(met_det * (x_i - x_j) @ torch.inverse(common_metric_t) @ (x_i - x_j).T)
 
+                    # symmetry reasons
                     self.distance_matrix[i, j] = mahalanobis_distance
                     self.distance_matrix[j, i] = mahalanobis_distance
+            pbar.update(1)
         
 
     # function to calculate the distance between any points
     def distance(self, 
-                 x: np.ndarray,
-                 y: Union[np.ndarray, None] = None,
+                 x: Union[int, np.ndarray],
+                 y: Union[int, np.ndarray, None] = None,
                  return_path: bool = False) -> float:
         '''
-        x_ind: np.ndarray               - index of the reference point
-        y_ind: Union[np.ndarray, None]  - index of the target point(s); None selects all points
-        return_path: bool               - return all points on the shortest path
+        x_ind: Union[int, np.ndarray]       - index of the reference point
+        y_ind: Union[int, np.ndarray, None] - index of the target point(s); None selects all points
+        return_path: bool                   - return all points on the shortest path
         '''
         
-        # prepare the points
-        x = torch.tensor(x, dtype=torch.float32).to(self.device).reshape(1, -1)
-        x_pushed = self.flow.transform(x, timesteps=self.timesteps)
+        # make x and array of shape (1)
+        if isinstance(x, int):
+            x = np.array([x])
+        assert len(x) == 1
+        x = x.reshape(1)
 
-        if y is not None:
-            y = torch.tensor(y, dtype=torch.float32).to(self.device).reshape(1, -1)
-            y_pushed = self.flow.transform(y, timesteps=self.timesteps)
-        else:
-            y = self.data
-            y_pushed = self.data_pushed
+        if y is None:
+            y = np.arange(self.data.shape[0])
+        elif isinstance(y, int):
+            y = np.array([y])
+        # make y and array of shape (len)
+        y = y.reshape(y.shape[0])
 
-        # generate new empty distance matrix with old entries
 
-        new_distance_matrix = torch.zeros((self.grid.shape[0]+y.shape[0]+1, self.grid.shape[0]+y.shape[0]+1)).to(self.device)
-        new_distance_matrix.fill_(float('inf'))
-
-        new_distance_matrix[:self.grid.shape[0], :self.grid.shape[0]] = self.distance_matrix
-
-        # for every y get the neighbous in the grid
-        y_indices = []
-        for i in range(y_pushed.shape[0]):
-            _, y_index = self.KDTree_grid_pushed.query(y_pushed[i].cpu().detach().numpy(), k=self.k_neighbours)
-            y_indices.append(y_index)
-
-        # get jacobian of every y
-        y_jacobians = np.zeros((y_pushed.shape[0], self.dimension, self.dimension))
-        for i in range(y_pushed.shape[0]):
-            J = self.jacobian(y[i].reshape(1, -1))
-            y_jacobians[i] = J.cpu().detach().numpy()
-        y_jacobians = torch.tensor(y_jacobians, dtype=torch.float32).to(self.device)
-
-        # expand the entries of the new distance matrix by directed edges from the grid to the y points
-        for i in range(y_pushed.shape[0]):
-            for j in y_indices[i]:
-                common_cov = (self.jacobians[j] + y_jacobians[i])/2 + 1e-6*torch.eye(self.dimension).to(self.device)
-
-                x_i = self.grid[j].reshape(1, -1)
-                x_j = y[i].reshape(1, -1)
-
-                cov_det = torch.det(common_cov) ** 1/self.dimension
-
-                mahalanobis_distance = torch.sqrt(cov_det * (x_i - x_j) @ torch.inverse(common_cov) @ (x_i - x_j).T)
-
-                new_distance_matrix[j, self.grid.shape[0]+i] = mahalanobis_distance
-
-        # concat the y_pushed and grid pushed and find the nearest neighbours of pushed x
-        all = torch.cat((self.grid, y), dim=0)
-        all_pushed = torch.cat((self.grid_pushed, y_pushed), dim=0) 
-        KDTree_all_pushed = KDTree(all_pushed.cpu().detach().numpy())
-        _, x_indices = KDTree_all_pushed.query(x_pushed[0].cpu().detach().numpy(), k=self.k_neighbours)
-
-        # if all inidices are not in the grid, increase the numbers of neighbours until at least 1 neighbour is in the grid
-        while len(set(x_indices).intersection(set(range(self.grid.shape[0]))) ) == 0:
-            _, x_indices = KDTree_all_pushed.query(x_pushed[0].cpu().detach().numpy(), k=self.k_neighbours+1)
-            self.k_neighbours += 1
-
-        # combine the jacobians
-        all_jacobians = torch.cat((self.jacobians, y_jacobians), dim=0)
-
-        # get jacobian of x
-        x_jacobian = self.jacobian(x)
-
-        # expand the entries of the new distance matrix by directed edges from the x point to the grid
-        for j in x_indices:
-            common_cov = (all_jacobians[j] + x_jacobian)/2 + 1e-6*torch.eye(self.dimension).to(self.device)
-
-            x_i = x.reshape(1, -1)
-            x_j = all[j].reshape(1, -1)
-
-            cov_det = torch.det(common_cov) ** 1/self.dimension
-
-            mahalanobis_distance = torch.sqrt(cov_det * (x_i - x_j) @ torch.inverse(common_cov) @ (x_i - x_j).T)
-
-            # dijkstra does not work with 0 distances, so set it to a small value
-            if mahalanobis_distance == 0:       
-                mahalanobis_distance = 1e-6
-
-            new_distance_matrix[-1, j] = mahalanobis_distance
-
-        self.new_distance_matrix = new_distance_matrix
-        # calculate the shortest path
-        distance_matrix_np = new_distance_matrix.cpu().detach().numpy()
-        dist, path = dijkstra(distance_matrix_np, directed=True, indices=-1, return_predecessors=True)
-
-        # round distance to 5 decimal places
-        dist = np.round(dist, 5)
-
-        if not return_path:
-            # filter out the grid distances
-            dist = dist[self.grid.shape[0]:-1]
-            return dist
+        # get the shortest path between the points using dijkstra
+        dist, path = dijkstra(self.distance_matrix.cpu().detach().numpy(), indices=x, return_predecessors=True)
         
-        if return_path and y is not None and y.shape[0] == 1:
-            
-            start_index = self.new_distance_matrix.shape[0]-1
-            end_index = self.new_distance_matrix.shape[0]-2
+        return_distances = np.array(dist[0, y])
+        
+        if return_path and len(y) == 1:
+            shortest_path = []
+            i = x[0]
+            j = y[0]
+            while j != i:
+                shortest_path.append(j)
+                j = path[0, j]
 
-            shortest_path = [end_index] 
-            while end_index != start_index:
-                end_index = path[end_index]
-                shortest_path.append(end_index)
+            shortest_path.append(i)
+            shortest_path.reverse()
 
-            # get the actual points of the indices
-            shortest_path = [all[i].cpu().detach().numpy() for i in shortest_path[:-1]]
-
-            #append x
             shortest_path = np.array(shortest_path)
-            shortest_path = np.concatenate((shortest_path, x.cpu().detach().numpy()))
+            shortest_path = self.reference[shortest_path]
 
-            return dist[-2], shortest_path
+            return return_distances, shortest_path
         
         else:
-            dist = dist[self.grid.shape[0]:-1]
-            return dist
+            return return_distances
 
-
+    # function to calculate the jacobian of the flow and the cdf transformation
     def jacobian(self, X: torch.Tensor) -> torch.Tensor:
         '''
         X: torch.Tensor - input data
@@ -300,26 +237,24 @@ class LAMINAR():
         return J.detach()
     
 
-    def query(self, 
+    def query(self,
               x: Union[int, np.ndarray],
-              k_neighbours: int = 0):
-        if k_neighbours == 0:
-            k_neighbours = self.k_neighbours
+              k_neighbours: Union[int, None] = None):
+        
+            if isinstance(x, int):
+                x = np.array([x])
+            x = x.reshape(1, -1)
 
-        return_indices = []
-        return_distances = []
+            if k_neighbours is None:
+                k_neighbours = self.k_neighbours
 
-        if isinstance(x, int):
-            x = np.array([x])
+            neighbours = []
+            distances = []
 
-        for i in range(x.shape[0]):
-            dist = self.distance(self.data[x[i]].detach().numpy(), return_path=False)
+            for i in x[0]:
+                dist = self.distance(int(i), return_path=False)
+                _, indices = torch.topk(torch.tensor(dist), k=k_neighbours, largest=False)
+                neighbours.append(indices.cpu().detach().numpy())
+                distances.append(dist[indices])
 
-            # sort data according to distance
-            sorted_indices = np.argsort(dist)
-            sorted_dist = dist[sorted_indices]
-
-            return_indices.append(sorted_indices[:k_neighbours])
-            return_distances.append(sorted_dist[:k_neighbours])
-
-        return return_indices, return_distances
+            return neighbours, distances
